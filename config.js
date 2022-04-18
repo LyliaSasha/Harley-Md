51a133d2663863c1b4e6e32bb99303e3e55764d0
LyliaSasha <lyliachan45@gmail.com>
2022-04-18 15:02:50 +0700

    Harleybotz

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Jika Error Chat Me
+   wa.me/6285795586847
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285795586847','6285795586847']
+global.web = 'https://www.youtube.com/channel/UCKTdQnHWeGgwiwz35zmduGg'
+global.bot = 'Harley-MD'
+global.premium = ['6285795586847']
+global.packname = 'SashaCh'
+global.author = 'SashaCh'
+global.sessionName = 'Harley'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./media/harley.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
